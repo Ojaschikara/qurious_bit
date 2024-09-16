5e93bc375ddcbd81da5076a79c399c75c96d7380
Ojaschikara <ojaschikara@email.com>
2024-09-16 17:46:01 +0530

    Added

---
+++ Backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const connection = require('./config/db')
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+ 
+
+
+// Routes
+app.use('/api/products', require('./routes/productsRoutes'));
+app.use('/api/discounts', require('./routes/discountsRoutes'));
+app.use('/api/cart', require('./routes/cartRoutes'));
+
+app.listen(port, async() => {
+   try {
+    await connection
+    console.log(`Server is running on port: ${port}`);
+   } catch (error) {
+    console.log(error)
+   }
+});
